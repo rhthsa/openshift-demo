ce8b3b92e47b9f644e4d34dac1c088954eea6230
chatapazar <chatapazar@gmail.com>
2021-07-09 12:17:22 +0700

    add thanos receive

---
+++ bin/start_build_pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+PROJECT=ci-cd
+PIPELINE=backend-build-pipeline
+OUTPUT=$(oc start-build ${PIPELINE} -n ${PROJECT})
+BUILD=$(echo ${OUTPUT}|awk '{print $1}'|awk -F'/' '{print $2}')
+echo "Build ${BUILD} started"
+echo "Wait 5 sec for ${BUILD} to be started"
+sleep 5
+oc logs build/${BUILD} -n ${PROJECT}
