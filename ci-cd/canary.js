956aa59998f7fc52c898bebde511df849a78950d
GitHub Action <github-action@users.noreply.github.com>
2021-09-13 01:15:25 +0000

    Github Actions Update Gitbook

--- ci-cd/canary.js
+++
@@ -1,6 +0,0 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-export default function () {
-  http.get('http://canary-bot.apps.cluster-852b.852b.example.opentlc.com/api/values/information');
-  sleep(1);
-}
