eda3bdd0ad1820540cc21c79726202fd35137ee2
GitHub Action <github-action@users.noreply.github.com>
2023-07-05 10:51:09 +0000

    Github Actions Update Gitbook

--- bin/count-pod-by-worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LABEL="app=backend,version=v1"
-ZONE_LABEL="zone"
-for node in $(oc get node -l node-role.kubernetes.io/worker="" --no-headers -o custom-columns='Name:.metadata.name')
-do
-   ZONE=$(oc get node $node -o jsonpath='{.metadata.labels.'$ZONE_LABEL'}')
-   COUNT=$(oc get po -l "${LABEL}" -o custom-columns='Node:.spec.nodeName'|grep $node|wc -l|awk '$1=$1')
-   echo "$node ($ZONE) => $COUNT"
-done
