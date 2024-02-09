0f5f057f61e82dbde81cdd037dc1bace5b8247fe
GitHub Action <github-action@users.noreply.github.com>
2024-02-09 09:37:26 +0000

    Github Actions Update Gitbook

--- bin/count-all-pods-by-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# oc get po -o wide --all-namespaces | grep ip-10-0-234-249.ap-southeast-1.compute.internal | wc -l
-for node in $(oc get node -l node-role.kubernetes.io/worker="" --no-headers -o custom-columns='Name:.metadata.name')
-do
-   COUNT=$(oc get po -l app=backendW@ -o wide --all-namespaces | grep $node| wc -l)
-   echo "$node => $COUNT pods"
-done
